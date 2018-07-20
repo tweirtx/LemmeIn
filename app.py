5809e1b9d8e5eaf48e74da7d9fa1bfa322b22c13
Travis Weir <tweirtx@gmail.com>
2018-07-20 01:44:18 -0500

    Works except for submit not submitting correctly

--- app.py
+++ app.py
@@ -1,2 +1,30 @@
 import flask
 import slackclient
+import os
+from flask import request
+
+slacktoken = os.getenv("slacktoken")
+slackchannel = os.getenv('slackchannel')
+
+app = flask.Flask('LemmeIn')
+slack = slackclient.SlackClient(token=slacktoken)
+
+
+@app.route('/')
+def index():
+    return flask.send_from_directory('.', 'index.html')
+
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    name = request.values.get('name')
+    door = request.values.get('door')
+    composed_string = "{} needs to be let in at the {} door".format(name, door)
+    call = slack.api_call(
+        "chat.postMessage",
+        channel=slackchannel,
+        text=composed_string
+    )
+    print(call)
+    print(composed_string)
+    return flask.send_from_directory('.', 'submitted.html')
