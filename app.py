abfa42f4e1aa5d48acf11c2cb61d6d5138c84ead
Travis Weir <tweirtx@gmail.com>
2020-01-25 14:15:59 -0600

    Updates to make this work

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-import slackclient
+import slack
 import os
 from flask import request
 
@@ -7,7 +7,7 @@ slacktoken = os.getenv("slacktoken")
 slackchannel = os.getenv('slackchannel')
 
 app = flask.Flask('LemmeIn')
-slack = slackclient.SlackClient(token=slacktoken)
+slackinteract = slack.WebClient(token=slacktoken)
 
 
 @app.route('/')
@@ -20,11 +20,14 @@ def submit():
     name = request.values.get('name')
     door = request.values.get('door')
     composed_string = "{} needs to be let in at the {} door".format(name, door)
-    call = slack.api_call(
-        "chat.postMessage",
+    call = slackinteract.chat_postMessage(
         channel=slackchannel,
         text=composed_string
     )
     print(call)
     print(composed_string)
     return flask.send_from_directory('.', 'submitted.html')
+
+
+if __name__ == "__main__":
+    app.run()
